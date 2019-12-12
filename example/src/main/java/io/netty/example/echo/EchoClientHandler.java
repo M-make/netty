a636b23efa4a007407c6fcd1e48202044992a009
jzdayz <1397649957@qq.com>
2019-12-12 10:48:02 +0800

    cn

--- example/src/main/java/io/netty/example/echo/EchoClientHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoClientHandler.java
@@ -46,12 +46,12 @@ public class EchoClientHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println(msg);
+        ctx.write(msg);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
        ctx.flush();
-       ctx.close();
     }
 
     @Override
